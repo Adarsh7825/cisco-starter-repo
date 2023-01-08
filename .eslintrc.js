42991d76302e42fc66d9522a96f6369a66823fb5
Adarsh7825 <109406757+Adarsh7825@users.noreply.github.com>
2023-01-08 17:18:26 +0530

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+      "no-var": ["error"],
+      "prefer-const": ["error"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"]
+    }
+};
